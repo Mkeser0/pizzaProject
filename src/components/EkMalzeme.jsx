f1b0cbeb1f2069ef022997a8e16a6af2a28710a7
Mustafa Keser <mustafakeser0@hotmail.com>
2025-03-27 18:06:08 +0300

    son rötuş

--- src/components/EkMalzeme.jsx
+++ src/components/EkMalzeme.jsx
@@ -1,19 +1,46 @@
 import React from "react";
-import { Label, FormGroup, Input } from "reactstrap";
+import { Label, FormGroup } from "reactstrap";
+import styled from "styled-components";
+const StyledCheckbox = styled.input.attrs({ type: "checkbox" })`
+  appearance: none;
+  width: 2rem;
+  height: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #faf7f2;
+  position: relative;
+  cursor: pointer;
+
+  &:checked {
+    background-color: #fdc913;
+    border-color: #fdc913;
+  }
+
+  &:checked::after {
+    content: "✔";
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 16px;
+    font-weight: bold;
+    color: #292929;
+  }
+`;
+
 function EkMalzeme(props) {
   const { items, handleChange } = props;
   return (
     <FormGroup
       check
-      style={{ display: "flex", alignItems: "center", gap: "1rem" }}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "1rem",
+        marginBottom: ".5rem",
+      }}
     >
-      <Input
-        style={{
-          transform: "scale(3)",
-          border: "none",
-          backgroundColor: "#FAF7F2",
-          accentColor: "red",
-        }}
+      <StyledCheckbox
         value={items}
         name={items}
         id={items}
